e07677006f70d30cc98ae4bd1889730387175ead
ahmed98adly <ahmedadly67@hotmail.com>
2024-06-26 21:19:50 +0000

    model implementation

--- models/sybil/utils/SybilRunner.py
+++ models/sybil/utils/SybilRunner.py
@@ -9,18 +9,39 @@ Email:  ahmed.hassan@maastrichtuniversity.nl
 -------------------------------------------------
 """
 
+from ast import Module
 import os, subprocess, shutil, json, csv
 import pandas as pd
-from mhubio.core import Instance, InstanceData, IO
+from mhubio.core import Instance, InstanceData, IO, Module, ValueOutput, Meta
 from mhubio.modules.runner.ModelRunner import ModelRunner
 
-class SybilRunner(ModelRunner):
+
+@ValueOutput.Name('sybilscores')
+@ValueOutput.Meta(Meta(key="value"))
+@ValueOutput.Label('SybilRiskScore')
+@ValueOutput.Type(float)
+@ValueOutput.Description('Sybil Risk Scores for 6 years.')
+class SybilScores(ValueOutput):
+   pass
+
+
+@ValueOutput.Name('sybilscores')
+@ValueOutput.Meta(Meta(key="value"))
+@ValueOutput.Label('SybilRiskScore')
+@ValueOutput.Type(float)
+@ValueOutput.Description('Sybil Risk Scores for 6 years.')
+class RiskScoresloc(ValueOutput):
+   pass
+
+
+class SybilRunner(Module):
     
     @IO.Instance()
     @IO.Input('in_data', 'dicom:mod=ct',  the='input dicom chest ct')
-    @IO.Output('risk_scores',  'sybil.csv', "csv:type=scores", the='predicted cancer risk')
+    @IO.Output('risk_scores',  'sybil.csv', "csv", the='predicted cancer risk')
+    @IO.OutputData('sybilscores', SybilScores, data='in_data', the='sybil scores')
 
-    def task(self, instance: Instance, in_data: InstanceData, risk_scores: InstanceData) -> None:
+    def task(self, instance: Instance, in_data: InstanceData, risk_scores: InstanceData, sybilscores: SybilScores) -> None: 
         
         # import sybil model
         from sybil import Serie, Sybil
@@ -33,10 +54,10 @@ class SybilRunner(ModelRunner):
     
         # Get risk scores
         serie = Serie(dcm_images)
-        scores = model.predict([serie])
-        
+        SybilScores.value = model.predict([serie])
+
         # save risk scores for 6 years for every instance
-        df = pd.DataFrame({'Scores': scores})
+        df = pd.DataFrame({'Scores': SybilScores.value })
         df.to_csv(risk_scores.abspath, index=False)
 
         
